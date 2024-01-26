3329614ae2cd4c03b0ead08d3b035b6b0b758ff7
Ayberk Bilgiç <lewidvailed@outlook.com>
2024-01-26 14:41:39 +0300

    no storyboard and scene delegate update

---
+++ Auth-Login-System/Auth-Login-System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auth-Login-System
+//
+//  Created by Ayberk Bilgiç on 26.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
